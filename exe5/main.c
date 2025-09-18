7f39ce70b028494783c1383b1a21b2641907bbe8
LKenji <lucaskamikawa@gmail.com>
2025-09-18 14:53:37 -0300

    ex5

--- exe5/main.c
+++ exe5/main.c
@@ -11,53 +11,64 @@
 #define LED_PIN_Y 10
 
 QueueHandle_t xQueueBtn;
+QueueHandle_t xQueueLedR;
+QueueHandle_t xQueueLedY;
 
 void btn_callback(uint gpio, uint32_t events) {
     if ((events & GPIO_IRQ_EDGE_FALL)) {
         BaseType_t xHigherPriorityTaskWoken = pdFALSE;
         uint8_t id;
 
-        if (gpio == BTN_PIN_R) {
-            id = 0;
-            xQueueSendFromISR(xQueueBtn, &id, &xHigherPriorityTaskWoken);
-        } else if (gpio == BTN_PIN_Y) {
-            id = 1;
-            xQueueSendFromISR(xQueueBtn, &id, &xHigherPriorityTaskWoken);
-        }
+        if (gpio == BTN_PIN_R) id = 0;
+        else if (gpio == BTN_PIN_Y) id = 1;
+        else return;
 
+        xQueueSendFromISR(xQueueBtn, &id, &xHigherPriorityTaskWoken);
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
+}
+
+void btn_task(void *p) {
+    uint8_t id;
+    while (true) {
+        if (xQueueReceive(xQueueBtn, &id, portMAX_DELAY) == pdTRUE) {
+            if (id == 0) {
+                xQueueSend(xQueueLedR, &id, portMAX_DELAY);
+            } else if (id == 1) {
+                xQueueSend(xQueueLedY, &id, portMAX_DELAY);
+            }
+        }
     }
 }
 
 void led_task(void *p) {
     uint8_t led_id = (uint8_t)(uintptr_t)p;
     uint led_pin = (led_id == 0) ? LED_PIN_R : LED_PIN_Y;
+    QueueHandle_t q = (led_id == 0) ? xQueueLedR : xQueueLedY;
 
     gpio_init(led_pin);
     gpio_set_dir(led_pin, GPIO_OUT);
     gpio_put(led_pin, 0);
 
     int blinking = 0;
-    uint8_t id;
+    uint8_t dummy;
 
     while (true) {
-        // Check if a button event arrived (non-blocking)
-        if (xQueueReceive(xQueueBtn, &id, 0) == pdTRUE) {
-            if (id == led_id) {
-                blinking = !blinking;
-                if (!blinking) {
-                    gpio_put(led_pin, 0);
-                }
+        if (xQueueReceive(q, &dummy, portMAX_DELAY) == pdTRUE) {
+            blinking = !blinking;
+            if (!blinking) {
+                gpio_put(led_pin, 0);
             }
         }
 
-        if (blinking) {
+        while (blinking) {
             gpio_put(led_pin, 1);
             vTaskDelay(pdMS_TO_TICKS(100));
             gpio_put(led_pin, 0);
             vTaskDelay(pdMS_TO_TICKS(100));
-        } else {
-            vTaskDelay(pdMS_TO_TICKS(10));
+
+            // Quebra para verificar se botão chegou
+            if (uxQueueMessagesWaiting(q) > 0) break;
         }
     }
 }
@@ -65,7 +76,9 @@ void led_task(void *p) {
 int main() {
     stdio_init_all();
 
-    xQueueBtn = xQueueCreate(32, sizeof(uint8_t));
+    xQueueBtn   = xQueueCreate(32, sizeof(uint8_t));
+    xQueueLedR  = xQueueCreate(4, sizeof(uint8_t));
+    xQueueLedY  = xQueueCreate(4, sizeof(uint8_t));
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -78,7 +91,7 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true);
 
-    // Create two LED tasks, passing their ID (0=red, 1=yellow)
+    xTaskCreate(btn_task, "BTN Task", 256, NULL, 1, NULL);
     xTaskCreate(led_task, "LED R", 256, (void *)0, 1, NULL);
     xTaskCreate(led_task, "LED Y", 256, (void *)1, 1, NULL);
 
